d1453a8212530dfe551b0fa5e1156c0e41cadfcc
SandroRGS <sandrito_gs@hotmail.com>
2023-01-21 22:22:44 -0500

    actaulizacion

--- LISTADEEJERCICIONRO2/Ejercicio1.py
+++ LISTADEEJERCICIONRO2/Ejercicio1.py
@@ -26,15 +26,3 @@ while True:
         break
     else:
         break
-print("EJERCICIO 4\n")
-
-def MayorValor():
-    x = input("Ingrese un numero: ")
-    y = input("Ingrese otro numero: ")
-
-    if x > y:
-        return x
-    else:
-        return y
-
-print(MayorValor())
---
+++ LISTADEEJERCICIONRO2/Ejercicio4.py
@@ -0,0 +1,12 @@
+print("EJERCICIO 4\n")
+
+def MayorValor():
+    x = input("Ingrese un numero: ")
+    y = input("Ingrese otro numero: ")
+
+    if x > y:
+        return x
+    else:
+        return y
+
+print(MayorValor())
